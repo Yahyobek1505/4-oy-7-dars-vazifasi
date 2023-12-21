e32527780460d2dbf39fb281e4439462447005c4
Yahyobek1505 <yahyohonorinboyev0105@gmail.com>
2023-12-21 23:51:44 +0500

    Homework is almost done!

--- script.js
+++ script.js
@@ -29,21 +29,46 @@
 // }
 // console.log(sumArr("Jami " + array));
 
-//<=======================> 3-masala <====================>
+// <=======================> 3-masala <====================>
 
 // let array = [2, 17, 5, 60, 41, 13, 63, 84];
-// let odds = [1,];
-// // let counter = 0;
-// function sumArr(arg) {
-//   for (let i = 0; i < arg.length; i++) {
-//     if (arg[i] % 2 == 1) {
-//       // console.log(arg[i]);
-//       odds.push(arg[i]);
-//       // counter++;
+// function sumToq(odds) {
+//   let toqSonlar = [];
+//   for (let i = 0; i < odds.length; i++) {
+//     if (odds[i] % 2 == 1) {
+//       toqSonlar.push(odds[i]);
+//       console.log(odds[i]);
 //     }
 //   }
-//   return odds;
+//   return toqSonlar;
 // }
-// console.log(sumArr(odds));
+// console.log("Toq sonlar: " + sumToq(array));
 
-// "Jami " + 
+// <=======================> 4-masala <====================>
+
+// let array = [28, 14, 57, 68, 41, 12, 63, 84];
+// function sumJuft(juft) {
+//   let juftSonlar = [];
+//   for (let i = 2; i < juft.length; i+=2) {
+//       juftSonlar.push(juft[i]);
+//       console.log(juft[i]);
+//   }
+//   return juftSonlar;
+// }
+// console.log("Juft sonlar: " + sumJuft(array));
+
+// <=======================> 5-masala <====================>
+
+
+// <=======================> 6-masala <====================>
+
+let array = [2, 11, 57, 7, 41, 5, 63, 17];
+function sumTub(juft) {
+  let juftSonlar = [];
+  for (let i = 0; i < juft.length; i++) {
+      juftSonlar.push(juft[i]);
+      console.log(juft[i]);
+  }
+  return juftSonlar;
+}
+console.log("Juft sonlar: " + sumJuft(array));
