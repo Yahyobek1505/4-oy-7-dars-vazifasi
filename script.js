6005fb6ac22bc4029310523a51ad24faad0ff289
Yahyobek1505 <yahyohonorinboyev0105@gmail.com>
2023-12-21 20:52:12 +0500

    Homework is start.

---
+++ script.js
@@ -0,0 +1,49 @@
+//<=======================> 1-masala <====================>
+
+// let array = [1, 2, 3, 4, 5]
+
+// function sumArr(arg) {
+//   let counter = 0;
+// for (let i = 0; i < arg.length; i++) {
+//   if (arg[i] % 2 == 1) {
+//      counter++;
+//   }
+// }
+// return counter;
+// }
+// console.log(sumArr(array));
+
+//<=======================> 2-masala <====================>
+
+// let array = [21,42,63,84]
+
+// function sumArr(arg) {
+//   let sum = 1;
+// for (let i = 0; i < arg.length; i++) {
+//   if (arg[i] % 3 == 0 && arg[i] % 7 == 0) {
+//      sum = sum * arg[i];
+//      console.log(arg[i]);
+//   }
+// }
+// return sum;
+// }
+// console.log(sumArr("Jami " + array));
+
+//<=======================> 3-masala <====================>
+
+// let array = [2, 17, 5, 60, 41, 13, 63, 84];
+// let odds = [1,];
+// // let counter = 0;
+// function sumArr(arg) {
+//   for (let i = 0; i < arg.length; i++) {
+//     if (arg[i] % 2 == 1) {
+//       // console.log(arg[i]);
+//       odds.push(arg[i]);
+//       // counter++;
+//     }
+//   }
+//   return odds;
+// }
+// console.log(sumArr(odds));
+
+// "Jami " + 
